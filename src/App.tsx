63fae06bcaed874169628cd3a1a9d79726be33ed
i-am-not-cada <c.a.d.agyemang@gmail.com>
2025-04-13 01:07:27 +0100

    Add household budgeting page, basic version for now, to be tweaked

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,6 @@
 import React from "react";
 import { Route, Routes } from "react-router-dom";
-import { HelloWorld } from "./views";
+import { HelloWorld, Budgeting } from "./views";
 import AboutUs from "./views/about_us";
 import Registration from "./views/Registration";
 
@@ -10,6 +10,7 @@ const App: React.FC = () => {
       <Route path="/" element={<HelloWorld />} />
       <Route path="/about" element={<AboutUs />} />
       <Route path="/registration" element={<Registration />} />
+      <Route path="/budgeting" element={<Budgeting />} />
       {/* Add more routes here as needed */}
     </Routes>
   );
